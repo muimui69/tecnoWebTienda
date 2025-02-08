8ad4247e02ae479db7fbebe43036601283cd7a54
muimui69 <moises_c2015@outlook.es>
2025-02-08 02:11:16 -0400

    completo

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,5 +1,5 @@
-export const GLOBAL={
-    url:'http://localhost:3000',
+export const GLOBAL = {
+    url: 'https://tecnowebp1backend.onrender.com',
     // url:'https://ecommerce-backend-production-bceb.up.railway.app',
-     cliente_id:'Aat1GXTAFWbFCeoLvA1UdTnSM30Qew6V5Iu9_cV0ynQJApoY18Lsirfm-SaoA8kHOVeft7NiFz8VNqw_'
+    cliente_id: 'Aat1GXTAFWbFCeoLvA1UdTnSM30Qew6V5Iu9_cV0ynQJApoY18Lsirfm-SaoA8kHOVeft7NiFz8VNqw_'
 }
